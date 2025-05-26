1d355973b44ba2e06a93f826c32f15cb01fcd591
krishnaprolink <krishna.prolink@gmail.com>
2025-05-26 18:31:27 +0530

    changes of YogaSection.jsx

--- src/pages/home/yogaSection.jsx
+++ src/pages/home/yogaSection.jsx
@@ -1,7 +1,11 @@
 import { Typography, Grid, Box } from "@mui/material";
 import bg2 from "../../assets/images/bg_2.jpg";
 import lotus from "../../assets/images/lotus.png";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 const YogaSection = () => {
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.down("lg"));
   return (
     <Grid
       container
@@ -10,7 +14,9 @@ const YogaSection = () => {
       spacing={4}
       justifyContent="center"
       alignItems="center"
+      gap={2}
       className="py-5"
+      paddingX={{ xs: 2, sm: 3, md: 4 }}
     >
       <Grid size={{ xs: 12, md: 6 }} display="flex" justifyContent="center">
         <Box
@@ -26,6 +32,7 @@ const YogaSection = () => {
         display="flex"
         flexDirection="column"
         justifyContent="center"
+        alignItems={isMdUp ? "center" : "flex-start"}
         px={{ xs: 2, sm: 3, md: 4 }}
       >
         <Typography
@@ -34,12 +41,15 @@ const YogaSection = () => {
           gutterBottom
           align="center"
           textTransform={"uppercase"}
-          textAlign={"left"}
+          textAlign={isMdUp ? "center" : "left"}
           className=" !font-bold"
         >
           Life in Divine Yoga{" "}
         </Typography>
-        <Box className="flex justify-around w-120">
+        <Box
+          className="flex justify-around"
+          alignItems={isMdUp ? "center" : "flex-start"}
+        >
           <img src={lotus} width={100} height={100} alt="Lotus" />
           <Typography variant="h6" className="!my-6 ps-5">
             "I am standing on my own altar, The Poses are my prayers"
@@ -49,7 +59,7 @@ const YogaSection = () => {
           variant="body1"
           color="text.secondary"
           align="center"
-          textAlign={"left"}
+          textAlign={isMdUp ? "center" : "left"}
         >
           Experience holistic wellness with expert guidance and peaceful
           surroundings.
@@ -59,7 +69,7 @@ const YogaSection = () => {
           color="text.secondary"
           align="center"
           className="whitespace-pre-line max-w-[80ch] mx-auto"
-          textAlign={"left"}
+          textAlign={isMdUp ? "center" : "left"}
         >
           Discover peace and strength through mindful yoga practice. Our classes
           help you connect body and mind for lasting wellness.
